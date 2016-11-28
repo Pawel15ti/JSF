db1587c0d67bdff8e859e8658a08ee8c608fc706
Pawelozdoba07 <Pawelozdoba07@gmail.com>
2016-11-28 17:46:39 +0100

    Stworzenie projektu i polaczenie z repozytorium

---
+++ src/test/java/pl/ozdoba/PawelApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ozdoba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PawelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
